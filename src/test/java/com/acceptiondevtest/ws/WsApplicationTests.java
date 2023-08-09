edef85fed75dfd132339eb2dd425f116430e9761
Carlos Bilby <carlos.bilby@gmail.com>
2023-08-08 22:02:51 -0300

    feat: initial structure

---
+++ src/test/java/com/acceptiondevtest/ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acceptiondevtest.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
